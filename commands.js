f5baeb756a2297d19a6eba980297cedf52dfa469
Pimpila <pimpila@gmail.com>
2017-03-06 14:35:42 -0500

    added cat and head commands

--- commands.js
+++ commands.js
@@ -1,29 +1,48 @@
 var fs = require('fs');
 
 module.exports = {
-  pwd: function () {
+  pwd: function (fileName) {
     process.stdout.write(process.env.PWD);
-    process.stdout.write('\nprompt >' )
+    process.stdout.write('\nprompt > ' )
   },
-  date: function() {
+  date: function(fileName) {
     var today = new Date();
     var formattedDate = today.toDateString() + " " + today.toTimeString();
     process.stdout.write(formattedDate)
-    process.stdout.write('\nprompt >' )
+    process.stdout.write('\nprompt > ' )
   },
-  ls: function() {
-    fs.readdir('.', function(err, files) {
+  ls: function(fileName) {
+    fs.readdir('.', function(err, fileName) {
       if (err) {
         throw err;
       }
-      files.forEach(function(file){
+      fileName.forEach(function(file){
         process.stdout.write(file.toString() + '\n');
       })
-      process.stdout.write('\nprompt >' )
+      process.stdout.write('\nprompt > ' )
     })
   },
-  echo : function(input) {
-    process.stdout.write(input);
-    process.stdout.write('\nprompt >' )
+  echo: function(fileName) {
+    process.stdout.write(fileName);
+    process.stdout.write('\nprompt > ' )
+  },
+  cat: function(fileName) {
+    fs.readFile(fileName, function(err, data) {
+      if (err) {
+        throw err;
+      }
+      process.stdout.write(data);
+      process.stdout.write('\nprompt > ' )
+    })
+  },
+  head: function(fileName) {
+    fs.readFile(fileName, function(err, data){
+      if (err) {
+        throw err;
+      }
+      var dataArr = data.toString().split("\n");
+      process.stdout.write(dataArr.slice(0, 9).join("\n"));
+      process.stdout.write('\nprompt > ' )
+    })
   }
 }
